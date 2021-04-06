4acb3b86fe3c1799f9f96719891066a635d569a7
Tanaka <tanaka012@outlook.com>
2021-04-06 12:56:38 -0300

    commit pre almoco

---
+++ trabalho/lab3/lab3_concorrente.c
@@ -0,0 +1,19 @@
+/* De:Lucas Tatsuya Tanaka */
+/* DRE:118058149 */
+/* Email:tanaka@dcc.ufrj.br */
+/* Github:https://github.com/tatsutanka */
+/* Data:06/04/2021*/
+
+
+#include<stdio.h>
+#include<pthread.h>
+
+
+#define NTHREADS
+
+// tarefa
+// main
+int main(void){
+
+ return 0;
+}
